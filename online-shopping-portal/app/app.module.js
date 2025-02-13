a5dd4a4f09683eaebd53f875d99ef583eda458ee
Sagarcs96 <75712299+Sagarcs96@users.noreply.github.com>
2025-02-13 14:02:34 +0530

    Delete online-shopping-portal directory

--- online-shopping-portal/app/app.module.js
+++
@@ -1,1 +0,0 @@
-angular.module('onlineShoppingPortal', ['ngRoute']);
--- online-shopping-portal/app/services/productService.js
+++
@@ -1,6 +0,0 @@
-angular.module('onlineShoppingPortal')
-    .service('productService', ['$http', function ($http) {
-        this.getProducts = function () {
-            return $http.get('/api/products');
-        };
-    }]);
